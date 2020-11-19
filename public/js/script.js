cc47e202397abdfa6292d47e8317730886aa3518
Aidanr50 <20323198@nescol.ac.uk>
2020-11-19 20:29:26 +0000

    ADD event listeners for buttons and API data caching to button class

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,5 @@
-const poke_container = 
-document.getElementById('poke_container');
-const pokemon_number = 893;
+const poke_container = document.getElementById('poke_container');
+const pokemon_number = 386;
 
 const fetchPokemon = async () => {
     for(let i=1; i<=pokemon_number; i++) {
@@ -14,7 +13,6 @@ const getPokemon = async id => {
     const res = await fetch(url);
     const pokemon = await res.json()
     createPokemonCard(pokemon)
-    
 }
 
 fetchPokemon()
@@ -36,13 +34,13 @@ function createPokemonCard(pokemon) {
         <p class="mr-2">#${pokemon.id}</p>
         <h3 class>${name}</h3>
 
-        <button class="absolute right-2">
+        <button class="${name} ${pokemon.id} ${pokemon.stats[0].base_stat} ${pokemon.stats[1].base_stat} ${pokemon.stats[2].base_stat} ${pokemon.stats[3].base_stat} ${pokemon.stats[4].base_stat} ${pokemon.stats[5].base_stat} ${pokemon.types[0].type.name} pkmnBtn absolute right-2">
             <span><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M8 2C11.3 2 14 4.7 14 8C14 11.3 11.3 14 8 14C4.7 14 2 11.3 2 8C2 4.7 4.7 2 8 2ZM8 1C4.15 1 1 4.15 1 8C1 11.85 4.15 15 8 15C11.85 15 15 11.85 15 8C15 4.15 11.85 1 8 1Z" fill="#83BD79"/>
             <path d="M12 7.5H8.5V4H7.5V7.5H4V8.5H7.5V12H8.5V8.5H12V7.5Z" fill="#83BD79"/>
             </svg>
             </span>
-        </button>    
+        </button> 
     `;
 
     pokemonEl.innerHTML = pokemonInnnerHTML
@@ -51,14 +49,14 @@ function createPokemonCard(pokemon) {
 }
 
 function searchPokemon() {
-    // Declare variables
-  var input, filter, poke_container, pokemon_items, a, i, txtValue;
+// Declare variables
+  let input, filter, poke_container, pokemon_items, a, i, txtValue;
   input = document.getElementById('search_input');
   filter = input.value.toUpperCase();
   poke_container = document.getElementById("poke_container");
   pokemon_items = poke_container.querySelectorAll('.pokemon');
 
-  // Loop through all list items, and hide those who don't match the search query
+// Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < pokemon_items.length; i++) {
     a = pokemon_items[i].getElementsByTagName("h3")[0];
     txtValue = a.textContent || a.innerText;
@@ -69,3 +67,14 @@ function searchPokemon() {
     }
   }
 }
+
+function addPkmnToTeam($this) {
+    var pkmnClass = $this.attr("class").split(' ')[0];
+    alert(pkmnClass);
+};
+
+$("#poke_container").on('click', 'button.pkmnBtn', function(e) {
+    addPkmnToTeam($(this));
+    e.preventDefault();
+});
+
